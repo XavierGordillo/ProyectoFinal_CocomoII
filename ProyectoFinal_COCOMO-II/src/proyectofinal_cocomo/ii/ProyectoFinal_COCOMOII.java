4ad1017089a16a45a60e348b3c50115fdcc685e9
XavierGordillo <dario.gordillo@unl.edu.ec>
2020-08-03 18:52:32 -0500

    Prueba

--- ProyectoFinal_COCOMO-II/src/proyectofinal_cocomo/ii/ProyectoFinal_COCOMOII.java
+++ ProyectoFinal_COCOMO-II/src/proyectofinal_cocomo/ii/ProyectoFinal_COCOMOII.java
@@ -5,47 +5,17 @@
  */
 package proyectofinal_cocomo.ii;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 /**
  *
  * @author xavie
  */
-public class ProyectoFinal_COCOMOII extends Application {
-    
-    @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hola Mundo'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+public class ProyectoFinal_COCOMOII {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        launch(args);
+        // TODO code application logic here
     }
     
 }
